b9acb222428ed60abb991258c0d2e907ded0920b
oseijoel6111 <oseijoel6111@gmail.com>
2023-11-26 12:23:20 -0500

    update: post creation integrated

--- simple-laravel-blog-post/app/Http/Controllers/PostController.php
+++ simple-laravel-blog-post/app/Http/Controllers/PostController.php
@@ -6,6 +6,8 @@ use App\Http\Requests\PostRequest;
 use App\Models\Category;
 use Illuminate\Http\Request;
 use Exception;
+use App\Models\Post;
+use Illuminate\Support\Str;
 
 class PostController extends Controller
 {
@@ -13,18 +15,31 @@ class PostController extends Controller
     {
         $categories = Category::where('status', 1)->get();
 
-        return view('frontend.pages.posts.create')->with(['categories'=> $categories]);
+        return view('frontend.pages.posts.create')->with(['categories' => $categories]);
     }
 
-    public function store(PostRequest $req){
-        try{
+    public function store(PostRequest $req)
+    {
+        try {
 
+            // Determine post category
+            $category  = Category::where('slug', $req->get('category'))->first();
+            if ($category) {
+                $post = Post::create([
+                    'title' => $req->get('title'),
+                    'short_description' => $req->get('short_description'),
+                    'body' => $req->get('description'),
+                    'slug' => Str::slug($req->get('title')),
+                    'category_id' => $category->id
+                ]);
+                // attached image to its post
+                $post->attachMedia($req->file('image'));
 
-            
-
-        }catch(Exception $ex){
+                return redirect()->route('home');
+            }
+        } catch (Exception $ex) {
+            dd($ex);
             return abort(404);
         }
     }
-
 }
